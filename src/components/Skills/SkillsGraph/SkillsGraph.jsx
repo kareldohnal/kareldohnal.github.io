778378684be6fb42310e064de919dcb5982277ee
Karel Dohnal <karel.dohnal@gmail.com>
2021-01-23 21:16:32 +0100

    Skill toggle

--- src/components/Skills/SkillsGraph/SkillsGraph.jsx
+++ src/components/Skills/SkillsGraph/SkillsGraph.jsx
@@ -2,23 +2,35 @@ import React, { useState } from 'react';
 import { Button } from '../../Button/Button';
 import Skillsjson from "../SkillsData.json";
 
-// import './SkillsGraph.css';
+import './SkillsGraph.css';
 
 export const SkillsGraph = () => {
   const [select, setSelect] = useState(Object.keys(Skillsjson)[0])
-  console.log(select)
 
   return (
     <>
       <div className="skillsgraph">
         <div className="skill-switch">
-          {Object.keys(Skillsjson).map((value, index) => {
+          {Object.keys(Skillsjson).map((value) => {
             return <Button key={value} className={`toggle ${select === value ? "active" : ""}`} textBtn={Skillsjson[value].category} id={value} onClick={() => setSelect(value)} />
           })}
         </div>
         <div className="skill-list">
-          SkillList
-          </div>
+          {Object.keys(Skillsjson).map((value) => {
+            return <ul key={value} className={`fade ${select === value ? "active" : ""}`}>
+              {Object.keys(Skillsjson[value].skills).map((item) => {
+                return <li key={item} className="skill-item">
+                  <div className="skill-name">{item}</div>
+                  <div className="skill-bar">
+                    <div className="skill-level" style={{ width: Skillsjson[value].skills[item] + '0%' }}>
+                      <div className="skill-label">{Skillsjson[value].skills[item]}/10</div>
+                    </div>
+                  </div>
+                </li>
+              })}
+            </ul>
+          })}
+        </div>
       </div>
     </>
   );

47f1742599696d066ebef7b019673d05f0dc5ff2
Karel Dohnal <karel.dohnal@gmail.com>
2021-01-23 15:32:29 +0100

    hierarchy correction

--- src/components/Skills/SkillsGraph/SkillsGraph.jsx
+++ src/components/Skills/SkillsGraph/SkillsGraph.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from '../../Button/Button';
-import Skillsjson from "../Skills.json";
+import Skillsjson from "../SkillsData.json";
 
 // import './SkillsGraph.css';
 

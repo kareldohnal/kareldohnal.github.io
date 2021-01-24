e60b6a5008870b2b005a9fba07ce1a9c97af6efa
Karel Dohnal <karel.dohnal@gmail.com>
2021-01-24 22:53:08 +0100

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
-// import './Footer.css';
+import './Footer.css';
 
 export const Footer = () => {
     return (
       <>
         <div className="footer">
-
+          <FontAwesomeIcon icon={faCode} /> with <FontAwesomeIcon icon={faHeart} />.
         </div>
       </>
     );

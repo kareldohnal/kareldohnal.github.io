696197480b8b59b24bb3e7b2c7fff8b05e1f29ef
Karel Dohnal <karel.dohnal@gmail.com>
2021-02-16 22:24:49 +0100

    basic structure for projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import projectsJSON from "../../data/projects.json";
 
-// import './Projects.css';
+import './Projects.css';
+
+const requestImage = require.context('../../data/projectsimg', true, /png$/);
 
 export const Projects = () => {
-    return (
-      <>
-        <div className="projects">
-
+  return (
+    <>
+      <div className="projects" id="projects">
+        <div className="projects-head">
+        <h4>Projects</h4>
+        <p>Click on the thumbnails below for details.</p>
         </div>
-      </>
-    );
+        <div className="projects-gal">
+          {Object.keys(projectsJSON).map((value) => {
+            return (
+              <div className="projects-gal-item">
+                <img key={value} src={requestImage(`./${value}.png`).default} alt={projectsJSON[value].name} />
+                <div className="projects-gal-item-name">{projectsJSON[value].name}</div>
+                <div className="projects-gal-item-pitch">{projectsJSON[value].pitch}</div>
+              </div>
+              )
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
